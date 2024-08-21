01b47e81f177d2f2653cfe8aa826626148a64b80
Ivan Cheban <ivancheban@gmail.com>
2024-08-21 17:15:24 +0300

    test

--- salary_bot.py
+++ salary_bot.py
@@ -9,7 +9,6 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-TOKEN = os.getenv('TOKEN')
 KYIV_TZ = pytz.timezone('Europe/Kyiv')
 
 def is_ukrainian_holiday(date):
