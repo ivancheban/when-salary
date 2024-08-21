afeb58884d15c82eadae37855764948f40ccc5da
Ivan Cheban <ivancheban@gmail.com>
2024-08-21 13:43:05 +0300

    test

--- salary_bot.py
+++ salary_bot.py
@@ -63,7 +63,7 @@ def main() -> None:
     application.add_handler(CommandHandler("when_salary", when_salary))
 
     # Schedule the publish_salary_info function to run at 10:30 AM every day
-    schedule.every().day.at("1:41").do(lambda: application.run_task(publish_salary_info))
+    schedule.every().day.at("13:44").do(lambda: application.run_task(publish_salary_info))
 
     # Bind to the port provided by Heroku
     application.run_polling()
